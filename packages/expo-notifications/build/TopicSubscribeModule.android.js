796f1ce068671a0ea51c7a24b0f33a8249eae6c6
Momtchil Momtchev <momtchil@momtchev.com>
2022-05-28 16:53:28 +0200

    fix the exported name

--- packages/expo-notifications/build/TopicSubscribeModule.android.js
+++ packages/expo-notifications/build/TopicSubscribeModule.android.js
@@ -1,3 +1,4 @@
 import { NativeModulesProxy } from 'expo-modules-core';
+console.log(NativeModulesProxy);
 export default NativeModulesProxy.TopicSubscribeModule;
 //# sourceMappingURL=TopicSubscribeModule.android.js.map
